ba1ced81bdfe5d75c2e0353c0715619236507482
midzer <midzer@gmail.com>
2019-10-31 19:09:26 +0100

    save images in file system

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var fs = require('fs');
 var app = require('express')();
 var http = require('http');
 var server = http.createServer(app);
@@ -9,8 +8,14 @@ var wss = new WebSocket.Server({ server });
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
+const imageDataURI = require('image-data-uri');
+
 const adapter = new FileSync('db.json');
 const db = low(adapter);
+
+const imageSrcMatch = /<img src='([^']+)/;
+const imageTypeMatch = /data:image\/([^;]+)/;
+const imagePath = './images/';
 
 // Set some defaults
 db.defaults({ log: [] })
@@ -19,18 +24,42 @@ db.defaults({ log: [] })
 wss.on('connection', function(socket) {
   console.log('a user connected');
   if (socket.readyState === WebSocket.OPEN) {
-    const msgs = db.get('log')
+    const messages = db.get('log')
                    .map('msg')
                    .value();
-    msgs.forEach(msg => {
-      socket.send(msg);
+    messages.forEach(message => {
+      const imageSrc = message.match(imageSrcMatch);
+      if (imageSrc) {
+        imageDataURI.encodeFromFile(imagePath + imageSrc[1])
+        .then(result => socket.send(`<img src='${result}'>`))
+      }
+      else {
+        socket.send(message);
+      }
     });
   }
   socket.on("message", function(message) {
-    db.get('log')
-      .push({ msg: message })
-      .write();
-    wss.clients.forEach(function(client) {       
+    const imageSrc = message.match(imageSrcMatch);
+    if (imageSrc) {
+      const now = new Date();
+      const fileName = now.toISOString().replace(/[^\w]/g, '');
+      const filePath = imagePath + fileName;
+      const imageType = imageSrc[1].match(imageTypeMatch);
+      if (imageType) {
+        imageDataURI.outputFile(imageSrc[1], filePath)
+        .then(result => {
+          db.get('log')
+            .push({ msg: `<img src='${fileName}.${imageType[1]}'>` })
+            .write();
+        });
+      }
+    }
+    else {
+      db.get('log')
+        .push({ msg: message })
+        .write();
+    }
+    wss.clients.forEach(function(client) {
       if (client !== socket && client.readyState === WebSocket.OPEN ) {
         client.send(message);
       }
